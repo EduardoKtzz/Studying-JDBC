a2dcd764b175861eafb2ba3cb384ede20a7bf917
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-05-31 19:18:58 -0300

    Aula 6 (Project DAO) update

--- Aula6/src/Application/Program.java
+++ Aula6/src/Application/Program.java
@@ -2,17 +2,30 @@ package Application;
 
 import Model.Dao.DaoFactory;
 import Model.Dao.SellerDao;
+import Model.Entities.Department;
 import Model.Entities.Seller;
+
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 
         //Chamando o metodo de criar usuário na classe Factory
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println("=== TESTE 1: Seller findById ===");
 
         //Usando o metodo de buscar vendedor por ID
+        System.out.println("=== TESTE 1: Seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        //Testando o metodo para buscar vendedores pelo id do departamento
+        System.out.println();
+        System.out.println("=== TESTE 2: Seller findByDepartment ===");
+        Department department = new Department(1, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
