258ef5ec3db1a2bde2429ab94f428d2629baf9c5
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-01 19:09:35 -0300

    Aula 6 (Project DAO) update

--- Aula6/src/Application/Program2.java
+++ Aula6/src/Application/Program2.java
@@ -1,7 +1,18 @@
 package Application;
 
+import Model.Dao.DaoFactory;
+import Model.Dao.DepartmentDao;
+
 public class Program2 {
     public static void main(String[] args) {
+
+        //Chamando a função para instanciar um novo objeto na memória
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        //Testando o metodo para inserir dados no banco de dados
+        System.out.println("=== TEST 1 - Inserir departamento novo ===");
+
+
 
     }
 }
--- Aula6/src/Model/Deo/impl/DepartmentDaoJDBC.java
+++ Aula6/src/Model/Deo/impl/DepartmentDaoJDBC.java
@@ -1,4 +1,43 @@
 package Model.Deo.impl;
 
-public class DepartmentDaoJDBC {
+import Model.Dao.DepartmentDao;
+import Model.Entities.Department;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    //Atributos
+    private final Connection conn;
+
+    //Construtor para inicializar a conexão
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
 }
